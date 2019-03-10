3285ccba99039c3a90cca004e816c7743aa004b3
samedii <samedii@gmail.com>
2019-03-10 21:31:54 +0100

    FeatureLabelDataset for ignite

--- didactic_meme/datasets.py
+++ didactic_meme/datasets.py
@@ -38,3 +38,52 @@ def train_validate_test_split(*data, validate_size=0.25, test_size=0.25, random_
     test_split = train_test_split(*data, test_size=test_size, random_state=random_state)
     validate_split = train_test_split(*test_split[0], test_size=validate_size/(1 - test_size), random_state=random_state)
     return validate_split[0], validate_split[1], test_split[1]
+
+
+class FeatureLabelDataset(torch.utils.data.Dataset):
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+
+    def __len__(self):
+        return len(list(self.features.values())[0])
+
+    def _get_item(self, d, index):
+        return {
+            key: value[index]
+            for key, value in
+            d.items()
+        }
+
+    def __getitem__(self, index):
+        return (
+            self._get_item(self.features, index),
+            self._get_item(self.labels, index),
+        )
+
+    # Hack to allow ignite.metrics.Loss to get the shape
+    class DictWithShape(dict):
+        @property
+        def shape(self):
+            return list(self.values())[0].shape
+
+    @staticmethod
+    def prepare_batch(batch, device, non_blocking):
+        return [
+            FeatureLabelDataset.DictWithShape(**{
+                key: ignite.engine.convert_tensor(value, device=device, non_blocking=non_blocking)
+                for key, value in b.items()
+            })
+            for b in batch
+        ]
+
+    @staticmethod
+    def batch_size(labels):
+        return list(labels.values())[0].shape[0]
+
+    @staticmethod
+    def get_batch_size_fn(sample_weight=None):
+        if sample_weight is None:
+            return FeatureLabelDataset.batch_size
+        else:
+            return lambda labels: labels[sample_weight].sum().item()
